5e2a18457f6f3aadf26c2013b891184926e70f2e
BalajiM5710 <109909788+BalajiM5710@users.noreply.github.com>
2024-06-02 20:06:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ for image_dir in image_dirs:
     image_files.extend([os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))])
 
 # Split images into chunks
-num_chunks = 5
+num_chunks = 2
 chunks = [image_files[i::num_chunks] for i in range(num_chunks)]
 
 # Function to get the chunk index for a user
